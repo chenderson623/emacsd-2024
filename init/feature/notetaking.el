689128c1026815fe164c1252830f33e10785f3e5
Chris Henderson <chris_henderson@swifttrans.com>
2025-05-30 07:52:17 -0700

    add denote packages

--- init/feature/notetaking.el
+++ init/feature/notetaking.el
@@ -5,8 +5,32 @@
   :commands (denote denote-open-or-create denote-link)
   :bind
   (("C-c n n" . denote)
-   ("C-c n f" . denote-open-or-create)
+   ("C-c n o" . denote-open-or-create)
    ("C-c n i" . denote-link)))
+
+;; https://github.com/protesilaos/consult-denote
+;; https://protesilaos.com/emacs/consult-denote
+(use-package consult-denote
+  :straight t
+  :bind
+  (("C-c n f" . consult-denote-find)
+   ("C-c n g" . consult-denote-grep))
+  :custom
+  (consult-denote-find-command #'consult-fd)
+  :config
+  (consult-denote-mode 1))
+
+;; https://github.com/namilus/denote-menu
+(use-package denote-menu
+  :straight t
+  :commands (list-denotes)
+  :bind ("C-c n l" . list-denotes)
+  :config
+  (define-key denote-menu-mode-map (kbd "c") #'denote-menu-clear-filters)
+  (define-key denote-menu-mode-map (kbd "/ r") #'denote-menu-filter)
+  (define-key denote-menu-mode-map (kbd "/ k") #'denote-menu-filter-by-keyword)
+  (define-key denote-menu-mode-map (kbd "/ o") #'denote-menu-filter-out-keyword)
+  (define-key denote-menu-mode-map (kbd "e") #'denote-menu-export-to-dired))
 
 (provide 'feature/notetaking)
 
