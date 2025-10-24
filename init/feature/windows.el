2ece42213de0493d74281556dc9e96c54c82c92a
Chris Henderson <chris_henderson@swifttrans.com>
2025-10-24 07:27:24 -0700

    add ace-window

--- init/feature/windows.el
+++ init/feature/windows.el
@@ -43,6 +43,30 @@
    ;;("C-M-<down>" . windmove-down)
    ))
 
+(use-package ace-window
+  :straight t
+  :bind
+  ("<f2>" . ace-window)
+  ("M-2" . ace-window)
+  :config
+
+  ;; (defvar aw-dispatch-alist
+  ;;   '((?x aw-delete-window "Delete Window")
+  ;;     (?m aw-swap-window "Swap Windows")
+  ;;     (?M aw-move-window "Move Window")
+  ;;     (?c aw-copy-window "Copy Window")
+  ;;     (?j aw-switch-buffer-in-window "Select Buffer")
+  ;;     (?n aw-flip-window)
+  ;;     (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+  ;;     (?c aw-split-window-fair "Split Fair Window")
+  ;;     (?v aw-split-window-vert "Split Vert Window")
+  ;;     (?b aw-split-window-horz "Split Horz Window")
+  ;;     (?o delete-other-windows "Delete Other Windows")
+  ;;     (?? aw-show-dispatch-help))
+  ;;   "List of actions for `aw-dispatch-default'.")
+
+  )
+
 ;;
 ;; Window related
 ;; https://github.com/abo-abo/hydra/wiki/Window-Management
