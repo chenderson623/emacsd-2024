d1a182149668435c08aeb4c33fe9a65a091425dd
Chris Henderson <chenderson623@gmail.com>
2026-09-05 10:17:31 -0700

    revamp autoloads

--- site-lisp/lib/autoloads.el
+++ site-lisp/lib/autoloads.el
@@ -8,16 +8,57 @@
 ;;---------------------------------------------------------------------
 ;; Autoload Functions
 ;;---------------------------------------------------------------------
+(defvar autoloads--registrations nil)
+(defvar autoloads--pending-registrations nil)
+(defvar autoloads--refresh-timer nil)
+
+(defun autoloads--refresh-registration (registration)
+  (let ((autoloads-file (cdr registration)))
+    (condition-case err
+        (progn
+          (message "Refresh autoloads: %s" autoloads-file)
+          (autoload:generate-loaddefs-file autoloads-file (car registration))
+          (load autoloads-file nil 'nomessage))
+      (error
+       (message "Could not refresh autoloads for %s: %s"
+                autoloads-file (error-message-string err))))))
+
+(defun autoloads--refresh-pending ()
+  (setq autoloads--refresh-timer nil)
+  (let ((registrations autoloads--pending-registrations))
+    (setq autoloads--pending-registrations nil)
+    (dolist (registration registrations)
+      (autoloads--refresh-registration registration))))
+
+(defun autoloads--after-save ()
+  (when buffer-file-name
+    (dolist (registration autoloads--registrations)
+      (when (and (not (equal (expand-file-name buffer-file-name)
+                             (cdr registration)))
+                 (seq-some (lambda (directory)
+                             (file-in-directory-p buffer-file-name directory))
+                           (car registration)))
+        (unless (member registration autoloads--pending-registrations)
+          (push registration autoloads--pending-registrations)))))
+  (when (and autoloads--pending-registrations
+             (not (timerp autoloads--refresh-timer)))
+    (setq autoloads--refresh-timer
+          (run-with-idle-timer 1 nil #'autoloads--refresh-pending))))
+
 (defun autoloads:define (path-list autoloads-filepath)
-  ;; if loaddefs does not exist, load defuns and build
-  (when (not (file-exists-p autoloads-filepath))
-    (message (format "Generate %S" autoloads-filepath))
+  (let ((registration
+         (cons (mapcar #'expand-file-name path-list)
+               (expand-file-name autoloads-filepath))))
+    (unless (member registration autoloads--registrations)
+      (push registration autoloads--registrations)
+      (add-hook 'after-save-hook #'autoloads--after-save)))
+  (unless (file-exists-p autoloads-filepath)
+    (message "Generate %S" autoloads-filepath)
     (autoload:generate-loaddefs-file autoloads-filepath path-list))
   (load autoloads-filepath nil t))
 
 (defun autoload:generate-loaddefs-file (loaddef-file-path autoload-dirs)
-  (loaddefs-generate autoload-dirs loaddef-file-path)
-  (byte-compile-file loaddef-file-path))
+  (loaddefs-generate autoload-dirs loaddef-file-path))
 
 (defun autoload:regenerate-loaddefs-file (loaddef-file-path autoload-dirs)
   (when (file-exists-p loaddef-file-path)
